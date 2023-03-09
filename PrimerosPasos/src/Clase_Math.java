84f3a8578c50e90755ac41ac22614a9f688754a6
wilsonfaura <wilsonfaura10@hotmail.com>
2023-03-09 16:05:18 -0500

    clase Math

---
+++ PrimerosPasos/src/Clase_Math.java
@@ -0,0 +1,12 @@
+
+public class Clase_Math {
+
+	public static void main(String[] args) {
+		//double raiz=Math.sqrt(9);
+				double base=5;
+				double exponente=3;
+				int resultado=(int)Math.pow(base, exponente);
+				System.out.println("El resultado de " + base + " elevado a "+ exponente + " es " + resultado);
+	}
+
+}
